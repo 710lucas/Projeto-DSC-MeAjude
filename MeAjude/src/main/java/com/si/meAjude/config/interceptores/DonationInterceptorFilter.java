5049025819e132ff595841733c76411665225acb
Anderson Alexsandro Soares <anderson.silva.04@outlook.com>
2023-10-25 12:50:54 -0300

    doação filtrada

--- MeAjude/src/main/java/com/si/meAjude/config/interceptores/DonationInterceptorFilter.java
+++ MeAjude/src/main/java/com/si/meAjude/config/interceptores/DonationInterceptorFilter.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Arrays;
 
@@ -16,14 +15,37 @@ public class DonationInterceptorFilter implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         User userFromRequest = getUserFromRequest(request);
         if(userFromRequest.getRole() == UserRole.ADMIN) return true;
+        if(hasOnlyIdInRequest(request) && userFromRequest.getId().equals(getIdFromUrl(request))) return true;
         if(hasUserIdInQuery(request)) if(userFromRequest.getId().equals(getUserIdInQuery(request))) return true;
         return sendForbiddenResponseAndReturnFalse(response);
     }
 
-
     private boolean sendForbiddenResponseAndReturnFalse(HttpServletResponse response) throws Exception {
         response.sendError(HttpServletResponse.SC_FORBIDDEN);
         return false;
+    }
+
+    private boolean hasOnlyIdInRequest(HttpServletRequest request){
+        String requestURI = request.getRequestURI();
+        String[] requestURISplit = requestURI.split("/");
+        if(requestURISplit.length != 3) return false;
+        if(isStringLong(requestURISplit[2])) return true;
+        return false;
+    }
+
+    private Long getIdFromUrl(HttpServletRequest request){
+        String requestURI = request.getRequestURI();
+        String[] requestURISplit = requestURI.split("/");
+        return Long.parseLong(requestURISplit[2]);
+    }
+
+    private boolean isStringLong(String s){
+        try{
+            Long.parseLong(s);
+            return true;
+        }catch (NumberFormatException e){
+            return false;
+        }
     }
 
     private User getUserFromRequest(HttpServletRequest request){
@@ -33,6 +55,7 @@ public class DonationInterceptorFilter implements HandlerInterceptor {
 
     private boolean hasUserIdInQuery(HttpServletRequest request){
         String requestURI = request.getQueryString();
+        if(requestURI == null) return false;
         String[] requestURISplit = requestURI.split("&");
         return Arrays.stream(requestURISplit).anyMatch(s -> s.contains("userId"));
     }
